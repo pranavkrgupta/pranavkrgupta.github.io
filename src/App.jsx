2450dbef7d52008d059e970883e037c75819ae91
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2024-12-15 16:53:47 +0530

    Add y padding to whole page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import "./index.css";
 import "./App.css";
 import AboutMe from "./components/AboutMe.jsx";
 import Education from "./components/Education.jsx";
 function App() {
   return (
-    <div className="bg-black text-white min-h-screen text-center">
+    <div className="bg-black text-white min-h-screen text-center py-5">
       <h1 className="text-2xl font-bold ">Hello! I'm Pranav Kumar Gupta</h1>
       <p>A Front End Developer | Web Developer | MERN stack Developer</p>
       <AboutMe />
